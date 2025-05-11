0057cfac1fbc9c6a08b5e971394630a9ea3ce1e7
kevburnsjr <kevburnsjr@gmail.com>
2025-05-11 16:20:28 -0700

    Rename connection to subscriptions

--- hub.go
+++ hub.go
@@ -15,19 +15,19 @@ type Hub interface {
 
 func newHub() *hub {
 	return &hub{
-		connections: make(map[string]map[*connection]bool),
-		broadcast:   make(chan message),
-		register:    make(chan *connection),
-		unregister:  make(chan *connection),
+		subscriptions: make(map[string]map[*connection]bool),
+		broadcast:     make(chan message),
+		register:      make(chan *connection),
+		unregister:    make(chan *connection),
 	}
 }
 
 type hub struct {
-	mutex       sync.RWMutex
-	connections map[string]map[*connection]bool
-	broadcast   chan message
-	register    chan *connection
-	unregister  chan *connection
+	mutex         sync.RWMutex
+	subscriptions map[string]map[*connection]bool
+	broadcast     chan message
+	register      chan *connection
+	unregister    chan *connection
 }
 
 func (h *hub) Run() {
@@ -36,23 +36,23 @@ func (h *hub) Run() {
 		case conn := <-h.register:
 			h.mutex.Lock()
 			for _, topic := range conn.topics {
-				if _, ok := h.connections[topic]; !ok {
-					h.connections[topic] = make(map[*connection]bool)
+				if _, ok := h.subscriptions[topic]; !ok {
+					h.subscriptions[topic] = make(map[*connection]bool)
 				}
-				h.connections[topic][conn] = true
+				h.subscriptions[topic][conn] = true
 			}
 			h.mutex.Unlock()
 		case conn := <-h.unregister:
 			h.mutex.Lock()
 			for _, topic := range conn.topics {
-				delete(h.connections[topic], conn)
+				delete(h.subscriptions[topic], conn)
 			}
 			close(conn.send)
 			h.mutex.Unlock()
 		case msg := <-h.broadcast:
 			h.mutex.RLock()
 			for _, t := range msg.Topics {
-				if connections, ok := h.connections[t]; ok {
+				if connections, ok := h.subscriptions[t]; ok {
 					for conn := range connections {
 						select {
 						case conn.send <- msg:
@@ -74,7 +74,7 @@ func (h *hub) Run() {
 func (h *hub) Connections() map[*connection]bool {
 	m2 := make(map[*connection]bool)
 	h.mutex.RLock()
-	for _, m := range h.connections {
+	for _, m := range h.subscriptions {
 		maps.Copy(m2, m)
 	}
 	h.mutex.RUnlock()
